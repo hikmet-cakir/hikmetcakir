53c40452b28f64104ed5b1fd9b4e78f9df1c2fee
Hikmet <hikmet_cakir@outlook.com>
2022-05-27 15:52:53 +0300

    Added Dynamic Fetching Essay Structure in FE Side's Spring Page

--- frontend/src/components/Spring.js
+++ frontend/src/components/Spring.js
@@ -1,47 +1,71 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import CssBaseline from '@mui/material/CssBaseline'; 
 import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
- 
+import Grid from '@mui/material/Grid'; 
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper'; 
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
-
-
-const StyledPaper = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(2),
-    maxWidth: 1000,
-    color: theme.palette.text.primary,
-  }));
-
-  const message = `Truncation should be conditionally applicable on this long line of text
-  as this is a much longer line than what the container can support. `;
+import Link from '@mui/material/Link';
 
 function Spring() {
+    const [essaySpring, setEssaySpring] = useState([]); 
+
+    useEffect(() => { fetchEssaysAboutJavaSE() }, []);
+
+    async function fetchEssaysAboutJavaSE() {
+        fetch('http://localhost:8080/essay/find/category/spring', {
+            method: 'GET',
+            headers:{
+                'Content-Type':'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(response => {
+            setEssaySpring(response.items);
+        })
+        .catch(error => {
+            console.log("Something went to wrong!");
+        }); 
+    };
+
+    const StyledPaper = styled(Paper)(({ theme }) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(2),
+        maxWidth: 1000,
+        color: theme.palette.text.primary,
+    }));
+
     return (
-       <div>
         <React.Fragment>
             <CssBaseline />
-            <Container fixed> 
-                <Box sx={{ flexGrow: 1, overflow: 'hidden', px: 3 }}>
-                    <StyledPaper sx={{ my: 1, mx: 'auto', p: 2 }}>
-                        <Grid container wrap="nowrap" spacing={4}>
-                        <Grid item>
-                            <Avatar>W</Avatar>
-                        </Grid>
-                        <Grid item xs zeroMinWidth>
-                            <Typography noWrap>{message}</Typography>
-                        </Grid>
-                        </Grid>
-                </StyledPaper> 
-                </Box> 
+            <Container fixed>
+                {
+                    essaySpring.length === 0 ? null : 
+                        essaySpring[0].map(essay => (
+                            <Box sx={{ flexGrow: 1, overflow: 'hidden', px: 3 }}>
+                                <StyledPaper sx={{ my: 1, mx: 'auto', p: 2 }}>
+                                    <Grid container wrap="nowrap" spacing={4}>
+                                    <Grid item>
+                                        <Avatar>W</Avatar>
+                                    </Grid>
+                                    <Grid item xs zeroMinWidth>
+                                        <Typography noWrap>{essay.title}</Typography>
+                                    </Grid>
+                                    <Grid item xs zeroMinWidth> 
+                                        <Link href="#" underline="hover">
+                                            Go to Essay
+                                        </Link>
+                                    </Grid>
+                                    </Grid>
+                                </StyledPaper> 
+                            </Box> 
+                        )) 
+                }
             </Container>
-        </React.Fragment>
-       </div>
+        </React.Fragment> 
     );
 }
 
