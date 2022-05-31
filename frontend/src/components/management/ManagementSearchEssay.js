756c5532e8c90416793b5187c0bcf260d46c591b
Hikmet <hikmet_cakir@outlook.com>
2022-05-31 16:25:01 +0300

    Added Search Page in Management Panel

--- frontend/src/components/management/ManagementSearchEssay.js
+++ frontend/src/components/management/ManagementSearchEssay.js
@@ -1,11 +1,46 @@
+import React, { useState } from 'react';
+import CssBaseline from '@mui/material/CssBaseline'; 
+import Container from '@mui/material/Container';    
+import Grid from '@mui/material/Grid';  
+import TextField from '@mui/material/TextField'; 
+import Button from '@mui/material/Button';
+import DatePicker from "react-datepicker"; 
 import ManagementHeader from "../../layouts/ManagementHeader";
+ 
 
-function ManagementSearchEssay() {
+import "react-datepicker/dist/react-datepicker.css";
+
+function ManagementSearchEssay() { 
+
+    const [startDate, setStartDate] = useState(new Date());
+
     return (
-        <div>
-            <ManagementHeader/>
-            <h1>Management Search Essay Page</h1>
-        </div>
+        <React.Fragment>
+        <ManagementHeader/>    
+        <CssBaseline />
+        <Container maxWidth="md">
+            <Grid container spacing={2} >
+                <Grid item xs={6}>     
+                    <TextField id="text-field-search-by-id" label="ID" variant="standard" />
+                    <Button variant="contained" size="medium">Search</Button>
+                </Grid> 
+                <Grid item xs={6}>     
+                    <TextField id="text-field-search-by-subject" label="Subject" variant="standard" />
+                    <Button variant="contained" size="medium" >Search</Button>
+                </Grid>
+                <Grid item xs={6}>     
+                    <TextField id="text-field-search-by-content" label="Content" variant="standard" />
+                    <Button variant="contained" size="medium" >Search</Button>
+                </Grid> 
+                <Grid item xs={3}>     
+                    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+                </Grid>
+                <Grid item xs={3}>
+                    <Button variant="contained" size="medium">Search</Button> 
+                </Grid>  
+            </Grid> 
+        </Container>
+        </React.Fragment>
     );
 }
 
--- frontend/src/layouts/ManagementHeader.js
+++ frontend/src/layouts/ManagementHeader.js
@@ -3,7 +3,7 @@ import './Header.css';
 function ManagementHeader() { 
     return ( 
         <div className="header">
-            <a className="name" href="/home">Management Panel</a>
+            <a className="name" href="/management-home">Management Panel</a>
             <div className="header-right"> 
                 <a className="header-right-content"  href="/management-search">Search Essay</a>
                 <a className="header-right-content"  href="/management-add">Add Essay</a> 
